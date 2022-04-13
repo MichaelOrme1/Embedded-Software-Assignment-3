5b0e4701ba7cb1d8556d7f88d5ee31e9977668d5
MichaelOrme1 <99344686+MichaelOrme1@users.noreply.github.com>
2022-04-13 15:09:11 +0100

    Add files via upload

---
+++ Assignment3.ino
@@ -0,0 +1,69 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+void Task1(){
+   digitalWrite(Watchdog,HIGH);//Send HIGH signal, turn LED on
+   delayMicroseconds(50);
+   digitalWrite(Watchdog,LOW);//Send LOW signal, turn LED off
+}
+
+void Task2(){
+  BUTTONstate = digitalRead(BUTTON); //Read button
+}
+
+void Task3(){
+  
+}
+
+void Task4and5(){
+  //Get analogue reading 4 times
+  int analogue1 = analogRead(ANALOGUE);
+  int analogue2 = analogRead(ANALOGUE);
+  int analogue3 = analogRead(ANALOGUE);
+  int analogue4 = analogRead(ANALOGUE);
+
+  average_analogue = (analogue1+analogue2+analogue3+analogue4)/4;//Get average analogue
+}
+
+
+
+
+void Task6(){
+  int i = 0;
+  while( i >1000){//Do 1000 times
+    __asm__ __volatile__ ("nop");
+    i++;
+  }
+}
+
+void Task7and8(){
+  int error_code;
+  if (average_analogue > 3300/2){
+    error_code = 1;
+    digitalWrite(LED,HIGH);//Send HIGH signal, turn LED on
+  }
+else{
+    error_code = 0;
+    digitalWrite(LED,LOW);//Send LOW signal, turn LED off
+
+  
+}
+}
+
+void Task9(){
+  //CSV
+  Serial.print(BUTTONstate);
+  Serial.print(",");
+  Serial.print(average_analogue);
+  Serial.print(",");
+  Serial.print(Frequency);
+    
+}
