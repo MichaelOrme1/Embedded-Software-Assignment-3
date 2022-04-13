15fa9eca380d9c4153fe2ac2028ea4b34877c70c
MichaelOrme1 <99344686+MichaelOrme1@users.noreply.github.com>
2022-04-13 16:37:53 +0100

    Integrated tasks into FreeRTOS

--- Assignment3.ino
+++ Assignment3.ino
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
 #include "esp_system.h"
+
+
 #define Watchdog 7
 #define LED 12
 #define BUTTON 3
 #define ANALOGUE 14
 
+
+void Task1( void *pvParameters );
+void Task2( void *pvParameters );
+void Task3( void *pvParameters );
+void Task4and5( void *pvParameters );
+void Task6( void *pvParameters );
+void Task7and8( void *pvParameters );
+void Task9( void *pvParameters );
+
 int average_analogue = 0;
 int BUTTONstate = 0;
 int Frequency = 0;
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -17,6 +34,56 @@ void setup() {
   pinMode(LED, OUTPUT);
   pinMode(BUTTON,INPUT);
   pinMode(ANALOGUE,INPUT);
+
+   xTaskCreate(
+    Task1
+    ,  "task1"   
+    ,  128  
+    ,  NULL
+    ,  1  
+    ,  NULL );
+    xTaskCreate(
+    Task2
+    ,  "task2"   
+    ,  128  
+    ,  NULL
+    ,  1  
+    ,  NULL );
+    xTaskCreate(
+    Task3
+    ,  "task3"   
+    ,  128  
+    ,  NULL
+    ,  1  
+    ,  NULL );
+    xTaskCreate(
+    Task4and5
+    ,  "task4and5"   
+    ,  128  
+    ,  NULL
+    ,  1  
+    ,  NULL );
+    xTaskCreate(
+    Task6
+    ,  "task6"   
+    ,  128  
+    ,  NULL
+    ,  1  
+    ,  NULL );
+    xTaskCreate(
+    Task7and8
+    ,  "task7and8"   
+    ,  128  
+    ,  NULL
+    ,  1  
+    ,  NULL );
+    xTaskCreate(
+    Task9
+    ,  "task9"   
+    ,  128  
+    ,  NULL
+    ,  1  
+    ,  NULL );
 
 }
 
@@ -26,21 +93,26 @@ void loop() {
 }
 
 
-void Task1(){
+void Task1(void *pvParameters ){
    digitalWrite(Watchdog,HIGH);//Send HIGH signal, turn LED on
-   delayMicroseconds(50);
+   vTaskDelay( 300 / portTICK_PERIOD_MS ); 
    digitalWrite(Watchdog,LOW);//Send LOW signal, turn LED off
 }
 
-void Task2(){
+void Task2(void *pvParameters ){
   BUTTONstate = digitalRead(BUTTON); //Read button
 }
 
-void Task3(){
+void Task3(void *pvParameters ){
+  while (digitalRead(1)== HIGH){
+    //Start timer 
+  }
+  //Stop timer
+  //Timer multiplied by frequency
   
 }
 
-void Task4and5(){
+void Task4and5(void *pvParameters ){
   //Get analogue reading 4 times
   int analogue1 = analogRead(ANALOGUE);
   int analogue2 = analogRead(ANALOGUE);
@@ -53,7 +125,7 @@ void Task4and5(){
 
 
 
-void Task6(){
+void Task6(void *pvParameters ){
   int i = 0;
   while( i >1000){//Do 1000 times
     __asm__ __volatile__ ("nop");
@@ -61,7 +133,7 @@ void Task6(){
   }
 }
 
-void Task7and8(){
+void Task7and8(void *pvParameters ){
   int error_code;
   if (average_analogue > 3300/2){
     error_code = 1;
@@ -75,8 +147,9 @@ else{
 }
 }
 
-void Task9(){
+void Task9(void *pvParameters ){
   //CSV
+  //Semaphores to wait for values
   Serial.print(BUTTONstate);
   Serial.print(",");
   Serial.print(average_analogue);
